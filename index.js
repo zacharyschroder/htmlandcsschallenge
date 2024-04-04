e59f10aa5d4e89ac2566a728b256e7e182fe3173
Zach <zachary@codespace.co.za>
2024-04-04 10:02:48 +0200

    added gitignore

---
+++ index.js
@@ -0,0 +1,11 @@
+// ghurhuehg
+
+/**
+ * dhiwhdwihdqihdiwqhdiwqhidohwiqodwq0idh
+ * @param {*} num1 
+ * @param {*} num2 
+ * @returns sum of numbers
+ */
+function addNumbers(num1, num2) {
+    return num1 + num2;
+}
